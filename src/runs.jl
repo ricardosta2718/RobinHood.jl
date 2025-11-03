c5c6e3c3a6c30be42559353e8614528093b721fc
ricardosta2718 <ricardo.sta2718@gmail.com>
2025-11-03 11:40:59 +0000

    runs.jl

--- src/runs.jl
+++ src/runs.jl
@@ -39,6 +39,7 @@ function Run_Multiple_Strings(filename,P_list,zstar_list)
     file = h5open("results/"*filename,"w")
     println("\nData file named ",filename,".h5 was created.")
     rmax = 8e1 # is cutoff
+    lmax = 10e1 #UV cutoff for energy regularization
     SNG(c, I) = action(c, I)
     cons(res, c, I) = (res .= [c[1], c[Int(end/2)], c[Int(end/2)+1], c[end]])
 
@@ -77,7 +78,7 @@ function Run_Multiple_Strings(filename,P_list,zstar_list)
                     prob = OptimizationProblem(optprob, c0, I,; lcons = eqconst, ucons = eqconst, lb = lbounds, ub = ubounds)
                     sol = solve(prob, IPNewton())#,g_tol=1e-12,x_tol=1e-4)
                     sols[i,:] = sol.u
-                    Eint[i] = SNG(sol.u,I)-2*rmax
+                    Eint[i] = SNG(sol.u,I)-2*lmax
                     if abs(sols[i,Int(end*3/4+0.5)]-(P/2))<0.15 && snapping==true
                         snap_flag=true
                     end
@@ -94,7 +95,7 @@ function Run_Multiple_Strings(filename,P_list,zstar_list)
                     prob2 = OptimizationProblem(optprob2, r0, I,; lcons = eqconst2, ucons = eqconst2, lb = lbounds2, ub = ubounds2)
                     sol2 = solve(prob2, IPNewton())
                     sols[i,:] = [sol2.u;z_ext]
-                    Eint[i] = SNG2(sol2.u,I)
+                    Eint[i] = SNG2(sol2.u,I)-2*lmax
                 end
             end
         file["P$(P)_z$(zstar)/Eint"]=Eint
@@ -109,3 +110,4 @@ function Run_Multiple_Strings(filename,P_list,zstar_list)
 end
 
 export Run_Multiple_Strings
+
